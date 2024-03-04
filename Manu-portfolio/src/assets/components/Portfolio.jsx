d1f02853627521cda3fbc643d66dd832683f402d
manuel pedrobelli <manupedrob@gmail.com>
2024-03-03 23:26:54 -0300

    adding metal store

--- Manu-portfolio/src/assets/components/Portfolio.jsx
+++ Manu-portfolio/src/assets/components/Portfolio.jsx
@@ -15,7 +15,7 @@ export default function Portfolio() {
             </div>
             
                 
-            <div id="trivia" className="flex flex-col items-center justify-center gap-5 bg-rgb-blue py-4 px-2 lg:w-4/5 md:flex-row md:rounded">
+            <div id="metal-store" className="flex flex-col items-center justify-center gap-5 bg-rgb-blue py-4 px-2 lg:w-4/5 md:flex-row md:rounded">
                 <div className="flex flex-col items-center justify-center text-center gap-4">
                     <h3 className="text-white font-bold text-2xl mb-2">Metal Guitar Store</h3>
                         <p className="text-white text-lg lg:px-10">The "Metal Guitar Store" is a front-end web app.<br/> <span className="font-bold text-yellow-800">HTML CSS JS </span></p>
